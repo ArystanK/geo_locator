f92af05f0adb472efc600674fad4d9d53b266c28
arctan <aarctan.k@gmail.com>
2021-12-07 13:44:17 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/kz/arctan/geo_locator/MainActivity.kt
@@ -0,0 +1,6 @@
+package kz.arctan.geo_locator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/weather_data_sourse/lib/weather_data_sourse.dart
@@ -0,0 +1,7 @@
+library weather_data_sourse;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
